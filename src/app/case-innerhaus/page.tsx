1f4835b246a75b655118a63919a3a70885621d69
Mateus Pacheco <mateuspachecosete@gmail.com>
2025-04-08 18:40:31 -0300

    fix: remove case innerhaus

--- src/app/case-innerhaus/page.tsx
+++
@@ -1,8 +0,0 @@
-import CasePage from "@/components/CasePage";
-import { innerhaus } from "@/helpers/CasesPage";
-
-export default function Page() {
-  return (
-    <CasePage data={innerhaus} />
-  );
-}

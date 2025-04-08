503277d135f0744a980a78aacbf0a6e8eb4ce7b2
Mateus Pacheco <mateuspachecosete@gmail.com>
2025-04-08 10:30:04 -0300

    feat: redirect from old site /cases to #cases

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config redirects */
+  async redirects() {
+    return [
+      {
+        source: "/cases",
+        destination: "/#cases",
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
